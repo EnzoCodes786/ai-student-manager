74f3bf4929d1c462668a9ed01bdbb9ca1546b7f1
Aryan Qayum <aryan.qayum666@gmail.com>
2026-07-17 23:49:55 +0530

    Completed Forgot Password API

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ require('@dotenvx/dotenvx').config()
 const app = require('./src/app')
 const PORT = process.env.SERVER_PORT || 4000
 
-
+    console.log("HOST:", process.env.SMTP_HOST);
+console.log("USER:", process.env.SMTP_USER);
+console.log("PASS:", process.env.SMTP_PASS?.slice(0, 5));
 app.listen(PORT,()=>{
     console.log('server is running at : http://localhost:4000')
 })
--- src/controllers/forgotPassword.controller.js
+++ src/controllers/forgotPassword.controller.js
@@ -1,8 +1,14 @@
 const pool = require('../database/db')
-
+const createOtp = require('../services/otpGen')
+const sendOTP = require('../services/mailService')
 async function forgotPassword(req,res) {
     const {email} = req.body;
-
+    const otp = await createOtp();
+    await sendOTP(email,otp)
+    
+    res.status(202).json({
+        message : "Otp sent !"
+    })
 }
 
 module.exports = {forgotPassword}
