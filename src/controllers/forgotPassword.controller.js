7805ac9eef4da3ba6a4ad53b3ba2513d9e6f7969
Aryan Qayum <aryan.qayum666@gmail.com>
2026-07-18 23:58:06 +0530

    Implemented verify Otp API

--- src/controllers/forgotPassword.controller.js
+++ src/controllers/forgotPassword.controller.js
@@ -1,14 +1,27 @@
-const pool = require('../database/db')
-const createOtp = require('../services/otpGen')
-const sendOTP = require('../services/mailService')
-async function forgotPassword(req,res) {
-    const {email} = req.body;
-    const otp = await createOtp();
-    await sendOTP(email,otp)
-    
-    res.status(202).json({
-        message : "Otp sent !"
-    })
+const pool = require("../database/db");
+const createOtp = require("../services/otpGen");
+const sendOTP = require("../services/mailService");
+const jwt = require('jsonwebtoken')
+async function forgotPassword(req, res) {
+  const { email } = req.body;
+  const otp = await createOtp();
+  await sendOTP(email, otp);
+  await pool.query(
+    `
+        INSERT INTO otp_table(otp,email)
+        VALUES(?,?)
+        `,
+    [otp, email],
+  );
+
+  const resetToken = jwt.sign({ email }, process.env.JWT_SECRET, {
+    expiresIn: "10m",
+  });
+
+  res.status(202).json({
+    message: "Otp sent !",
+    resetToken
+  });
 }
 
-module.exports = {forgotPassword}
+module.exports = { forgotPassword };
---
+++ src/controllers/verifyOtp.controller.js
@@ -0,0 +1,32 @@
+const pool = require('../database/db')
+const jwt = require('jsonwebtoken')
+async function verifyOtp(req,res) {
+    const {resetToken,otp} = req.body;
+    
+    let email
+    try {
+        const decoded = jwt.verify(resetToken,process.env.TOKEN_SECRET_KEY)
+        email = decoded.email
+    } catch (error) {
+        console.log(error)
+    }
+    const data = await pool.query(`
+        SELECT otp FROM otp_table
+        WHERE email = ?
+        `,[email])
+
+    
+    if (data.length === 0) {
+        return res.status(400).json({ message: "Invalid OTP" });
+    }
+
+    await pool.query(`DELETE FROM otp_table WHERE email = ?`, [email]);
+
+    const resetPasswordToken = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: '10m' });
+    res.status(202).json({
+        message:"Otp verified",
+        resetPasswordToken
+    })
+    }
+    
+    module.exports = {verifyOtp}
