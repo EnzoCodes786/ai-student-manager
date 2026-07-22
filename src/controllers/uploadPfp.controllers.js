cd96d68ae8f077e025ef6126083900f6d2f83344
Aryan Qayum <aryan.qayum666@gmail.com>
2026-07-22 20:59:48 +0530

    Implemented Cookied in pfp

--- src/controllers/uploadPfp.controllers.js
+++ src/controllers/uploadPfp.controllers.js
@@ -1,17 +1,25 @@
 const pool = require('../database/db')
+const jwt = require('jsonwebtoken')
 async function uploadPfp(req, res) {
     try {
         const user_id = req.user.user_id
-        
+        const pfpToken = req.cookies.pfpToken
+        if(!pfpToken){
+            return res.status(400).status.json({message : "pfpToken is missing"})
+        }
         if (!req.file) {
             return res.status(400).json({ message: 'No file uploaded' })
         }
+        let file
 
+        file = jwt.verify(pfpToken,process.env.TOKEN_SECRET_KEY)
+        const user_email = file.user_email
+        
         const imageUrl = req.file.path
 
         await pool.query(
-            'UPDATE user_model SET user_image = ? WHERE user_id = ?',
-            [imageUrl, user_id]
+            'UPDATE user_model SET user_image = ? WHERE user_email = ?',
+            [imageUrl, user_email]
         )
 
         res.json({
--- src/controllers/userSignup.controller.js
+++ src/controllers/userSignup.controller.js
@@ -1,16 +1,31 @@
-const pool = require('../database/db')
-const encrypt_password = require('../middlewares/passEncryption')
-const date = new Date()
-async function userSignup(req,res) {
-    const {user_name,user_email,user_pass,user_exam} = req.body;
-    const password =await encrypt_password(user_pass);
-   const data= await pool.query(`
+const pool = require("../database/db");
+const encrypt_password = require("../middlewares/passEncryption");
+const jwt = require("jsonwebtoken");
+const date = new Date();
+async function userSignup(req, res) {
+  const { user_name, user_email, user_pass, user_exam } = req.body;
+  const password = await encrypt_password(user_pass);
+  const data = await pool.query(
+    `
         INSERT INTO user_model(user_name,user_email,user_pass,user_exam,created_at)
         VALUES (?,?,?,?,?);
-        `,[user_name,user_email,password,user_exam,date]);
-    res.json({
-        message:"User registered ",
-        data : data[0]
-    })
+        `,
+    [user_name, user_email, password, user_exam, date],
+  );
+  const pfpToken = jwt.sign(
+    { user_email, purpose: "pfp" },
+    process.env.TOKEN_SECRET_KEY,
+    { expiresIn: "10m" },
+  );
+  res.cookie("pfpToken", pfpToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 10 * 60 * 1000,
+  });
+  res.json({
+    message: "User registered ",
+    data: data[0],
+  });
 }
-module.exports = {userSignup}
+module.exports = { userSignup };
